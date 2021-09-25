b0154be2dd96407ade8d6420a665e1f91643b4ed
Lartey Wahab Hakim <larteysurviver@gmail.com>
2021-09-25 12:38:03 +0100

    fcm

---
+++ 2/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+    "use strict";
+
+
+    /* ==========================================================================
+   Sub Form   
+   ========================================================================== */
+    
+    
+    
+    $('#mc-form').ajaxChimp({
+        language: 'cm',
+        url: 'http://csmthemes.us3.list-manage.com/subscribe/post?u=9666c25a337f497687875a388&id=5b881a50fb'
+    //http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+    });
+    
+    
+    $.ajaxChimp.translations.cm = {
+        'submit': 'Submitting...',
+        0: '<i class="fa fa-envelope"></i> Awesome! We have sent you a confirmation email',
+        1: '<i class="fa fa-exclamation-triangle"></i> Please enter a value',
+        2: '<i class="fa fa-exclamation-triangle"></i> An email address must contain a single @',
+        3: '<i class="fa fa-exclamation-triangle"></i> The domain portion of the email address is invalid (the portion after the @: )',
+        4: '<i class="fa fa-exclamation-triangle"></i> The username portion of the email address is invalid (the portion before the @: )',
+        5: '<i class="fa fa-exclamation-triangle"></i> This email address looks fake or invalid. Please enter a real email address'
+    }
+
+
+
+
+
+
+});
